186684b497ccd9df69952e3a29508e6fa2c35765
Ahmed Osman <a.osman@alustudent.com>
2026-03-12 00:21:00 -0700

    Updated tasks 0, 1 and 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            print("{}".format(chr(ord(c) - 32)), end="")
-        else:
-            print("{}".format(c), end="")
-    print()
+        c = ord(c)
+        if ((c >= 97) and (c <= 122)):
+            c -= 32
+        print("{}".format(chr(c)), end='')
+    print("")
